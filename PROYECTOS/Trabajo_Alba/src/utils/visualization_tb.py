5bf862930dd036011cbc58e1d9a9058fb65a5f6c
albalexia <albavargasarevalo90@gmail.com>
2020-08-31 22:57:56 +0200

    31

--- PROYECTOS/Trabajo_Alba/src/utils/visualization_tb.py
+++ PROYECTOS/Trabajo_Alba/src/utils/visualization_tb.py
@@ -5,9 +5,9 @@
 def show_pie(df, figsize=(15, 15), subplots=True, autopct = '%1.1f%%', legend=None, title=None, **argumentos_extra):
     return df.plot.pie( figsize=figsize,subplots=subplots, autopct = autopct, legend=legend, title=title, **argumentos_extra)
 
-
-def show_bar(df, figsize=(15, 15), subplots=True, autopct = '%1.1f%%', legend=None, title=None, **argumentos_extra):
-    return df.plot.bar( figsize=figsize,subplots=subplots, autopct = autopct, legend=legend, title=title, **argumentos_extra)
+vu.show_pie(pie_data, title="Paro por distrito")
 
 
-
+def show_bar( plt.rcParams["figure.figsize"] = [15, 10], index=None, ax = sns.barplot(y=None, x = None, data = None, palette=("BuGn_r")),
+fig.set_size_inches(30, 30), plt.xticks(rotation=84)):
+    return 
